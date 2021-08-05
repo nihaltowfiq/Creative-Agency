70ba0947fb35472a77cb355b67e1e85070f33296
nihaltowfiq <nihaltowfiq@gmail.com>
2021-08-06 03:18:50 +0600

    updated MakeAdmin page

--- src/components/templates/Admin/MakeAdmin.jsx
+++ src/components/templates/Admin/MakeAdmin.jsx
@@ -1,69 +1,40 @@
-import { useState } from 'react';
 import { Button, Col, Container, Form } from 'react-bootstrap';
 import { SidebarLayout } from '../../others';
 
-export const MakeAdmin = () => {
-    const [admin, setAdmin] = useState({});
-    const handleBlur = (e) => {
-        const newAdmin = { ...admin };
-        newAdmin[e.target.name] = e.target.value;
-        setAdmin(newAdmin);
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        fetch('https://dry-ocean-34765.herokuapp.com/addAdmin', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(admin),
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                if (data) {
-                    document.getElementById('makeAdminForm').innerHTML =
-                        '<h3 class="text-center text-success mt-5"><b>Admin Added Successfully</b></h3>';
-                }
-            });
-    };
-
+export const MakeAdmin = ({ changeHandler, submitHandler }) => {
     return (
         <SidebarLayout navFor="admin">
             <Container
                 className="mt-2 py-3"
                 style={{ backgroundColor: '#F4F7FC', height: '600px' }}
             >
-                <div className="mt-3 ml-3">
-                    <Form
-                        id="makeAdminForm"
-                        onSubmit={handleSubmit}
-                        className="p-5 mr-5 bg-white"
-                        style={{ borderRadius: '10px' }}
-                    >
-                        <Form.Label>
-                            <b>Email</b>
-                        </Form.Label>
-                        <Form.Row>
-                            <Form.Group as={Col}>
-                                <Form.Control
-                                    onBlur={handleBlur}
-                                    name="email"
-                                    size="lg"
-                                    type="text"
-                                    placeholder="example@email.com"
-                                    required
-                                />
-                            </Form.Group>
-                            <Form.Group as={Col}>
-                                <Button
-                                    variant="success"
-                                    type="submit"
-                                    size="lg"
-                                >
-                                    Submit
-                                </Button>
-                            </Form.Group>
-                        </Form.Row>
-                    </Form>
-                </div>
+                <Form
+                    id="makeAdminForm"
+                    className="p-5 mr-5 mt-3 ml-3 bg-white"
+                    style={{ borderRadius: '10px' }}
+                    onSubmit={submitHandler}
+                >
+                    <Form.Label>
+                        <b>Email</b>
+                    </Form.Label>
+                    <Form.Row>
+                        <Form.Group as={Col}>
+                            <Form.Control
+                                name="email"
+                                size="lg"
+                                type="email"
+                                onChange={changeHandler}
+                                placeholder="example@email.com"
+                                required
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col}>
+                            <Button variant="success" type="submit" size="lg">
+                                Submit
+                            </Button>
+                        </Form.Group>
+                    </Form.Row>
+                </Form>
             </Container>
         </SidebarLayout>
     );
--- src/pages/MakeAdmin.jsx
+++ src/pages/MakeAdmin.jsx
@@ -1,10 +1,36 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { MakeAdmin as MakeAdminComponent } from '../components/templates';
 
 export const MakeAdmin = () => {
+    const [admin, setAdmin] = useState({});
+
+    const handleChange = (e) => {
+        const newAdmin = { ...admin };
+        newAdmin[e.target.name] = e.target.value;
+        setAdmin(newAdmin);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('https://dry-ocean-34765.herokuapp.com/addAdmin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(admin),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data) {
+                    document.getElementById('makeAdminForm').innerHTML =
+                        '<h3 class="text-center text-success mt-5"><b>Admin Added Successfully</b></h3>';
+                }
+            });
+    };
     return (
         <Fragment>
-            <MakeAdminComponent />
+            <MakeAdminComponent
+                changeHandler={handleChange}
+                submitHandler={handleSubmit}
+            />
         </Fragment>
     );
 };
