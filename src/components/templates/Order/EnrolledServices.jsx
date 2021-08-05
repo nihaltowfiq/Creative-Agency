277c7de552b0ad8ddccba8a115c0d2e45c30af1c
nihaltowfiq <nihaltowfiq@gmail.com>
2021-08-06 03:12:43 +0600

    updated EnrolledServices page

--- src/components/templates/Order/EnrolledServices.jsx
+++ src/components/templates/Order/EnrolledServices.jsx
@@ -1,35 +1,21 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useContext, useState, useEffect } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import './EnrolledServices.css';
-import { UserContext } from '../../../App';
 import { SidebarLayout } from '../../others';
 
-export const EnrolledServices = () => {
-    const [orders, setOrders] = useState([]);
-    const [loggedInUser] = useContext(UserContext);
-
-    useEffect(() => {
-        fetch(
-            `https://dry-ocean-34765.herokuapp.com/orders?email=${loggedInUser.email}`
-        )
-            .then((res) => res.json())
-            .then((data) => setOrders(data));
-    }, []);
-
+export const EnrolledServices = ({ data }) => {
     return (
         <SidebarLayout navFor="customer">
             <Container
                 className="mt-2 py-3"
                 style={{ backgroundColor: '#F4F7FC' }}
             >
-                {orders.length === 0 && (
+                {data.length === 0 && (
                     <h4 className="my-4 text-center text-danger">
                         Loading Enrolled Services....
                     </h4>
                 )}
                 <Row>
-                    {orders.map((order) => (
+                    {data.map((order) => (
                         <Col key={order._id} md={5}>
                             <div className="enrolled-card mx-2 my-2">
                                 <Row>
--- src/pages/EnrolledServices.jsx
+++ src/pages/EnrolledServices.jsx
@@ -1,10 +1,22 @@
-import { Fragment } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
+import { UserContext } from '../App';
 import { EnrolledServices as EnrolledServicesCompoment } from '../components/templates';
 
 export const EnrolledServices = () => {
+    const [orders, setOrders] = useState([]);
+    const [loggedInUser] = useContext(UserContext);
+
+    useEffect(() => {
+        fetch(
+            `https://dry-ocean-34765.herokuapp.com/orders?email=${loggedInUser.email}`
+        )
+            .then((res) => res.json())
+            .then((data) => setOrders(data));
+    }, [loggedInUser.email]);
+
     return (
         <Fragment>
-            <EnrolledServicesCompoment />
+            <EnrolledServicesCompoment data={orders} />
         </Fragment>
     );
 };
