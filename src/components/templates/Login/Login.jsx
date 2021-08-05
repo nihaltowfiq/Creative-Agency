f787e41f62dd62c471e9b822a0fdeb171e0118b5
nihaltowfiq <nihaltowfiq@gmail.com>
2021-08-05 20:15:27 +0600

    changes in login

--- src/components/templates/Login/Login.jsx
+++ src/components/templates/Login/Login.jsx
@@ -1,42 +1,8 @@
-/* eslint-disable no-unused-vars */
-import { useContext } from 'react';
 import { Button, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Login.css';
-import { Link, useHistory, useLocation } from 'react-router-dom';
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import { UserContext } from '../../../App';
-import { firebaseConfig } from '../../../libs/api';
 
-export const Login = () => {
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    const history = useHistory();
-    const location = useLocation();
-    const { from } = location.state || { from: { pathname: '/' } };
-
-    if (firebase.apps.length === 0) {
-        firebase.initializeApp(firebaseConfig);
-    }
-    const handleGoogle = () => {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        firebase
-            .auth()
-            .signInWithPopup(provider)
-            .then((result) => {
-                const { displayName: name, email, photoURL: img } = result.user;
-                setLoggedInUser({ name, email, img });
-                localStorage.setItem(
-                    'user',
-                    JSON.stringify({ name, email, img })
-                );
-                history.replace(from);
-            })
-            .catch((error) => {
-                const errorMessage = error.message;
-                console.log(errorMessage);
-            });
-    };
-
+export const Login = ({ loginHandler }) => {
     return (
         <Container className="text-center">
             <Link to="/home">
@@ -50,7 +16,7 @@ export const Login = () => {
             <Row className="justify-content-md-center mt-3">
                 <div className="login-card mt-5">
                     <h3>Login With</h3>
-                    <Button onClick={handleGoogle} variant="light">
+                    <Button onClick={loginHandler} variant="light">
                         <img src="/images/icons/google.png" alt="" /> Continue
                         with Google
                     </Button>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,43 @@
-import { Fragment } from 'react';
+/* eslint-disable no-unused-vars */
+import { Fragment, useContext } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import 'firebase/auth';
+import * as firebase from 'firebase/app';
 import { Login as LoginComponent } from '../components/templates';
+import { UserContext } from '../App';
+import { firebaseConfig } from '../libs/api';
 
 export const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: '/' } };
+
+    if (firebase.apps.length === 0) {
+        firebase.initializeApp(firebaseConfig);
+    }
+    const handleGoogleSignIn = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase
+            .auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                const { displayName: name, email, photoURL: img } = result.user;
+                setLoggedInUser({ name, email, img });
+                localStorage.setItem(
+                    'user',
+                    JSON.stringify({ name, email, img })
+                );
+                history.replace(from);
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                console.log(errorMessage);
+            });
+    };
     return (
         <Fragment>
-            <LoginComponent />
+            <LoginComponent loginHandler={handleGoogleSignIn} />
         </Fragment>
     );
 };
